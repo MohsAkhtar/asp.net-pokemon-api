10161d8bab59d16e231cc943cda30b5281b54848
40183396 <40183396@live.napier.ac.uk>
2018-08-13 20:25:50 +0100

    Created Pokemon Model class. Created object in HomeController which connects json to it so we can access each pokemons data.

--- Pokemon API/Controllers/HomeController.cs
+++ Pokemon API/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Web.Mvc;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Pokemon_API.Models;
 
 namespace Pokemon_API.Controllers
 {
@@ -8,9 +12,28 @@ namespace Pokemon_API.Controllers
         public ActionResult Index()
         {
             // Create request to API
-            WebRequest request = WebRequest.Create("http://pokeapi.co/api/v2/pokemon/10");
+            WebRequest request = WebRequest.Create("http://pokeapi.co/api/v2/pokemon/1");
             // Send that request off
             WebResponse response = request.GetResponse();
+
+            // Function that returns a stream of information
+            // Get back the response stream
+            Stream stream = response.GetResponseStream();
+            // Make it accessible
+            StreamReader reader = new StreamReader(stream);
+            
+            // Reads all of information in StreamReader and put in it string
+            // Which is json formatted
+            string responseFromServer = reader.ReadToEnd();
+            // JObject is a way of parsing jSon data
+            // Turns into jObject a much more readable json
+            JObject parsedString = JObject.Parse(responseFromServer);
+           
+            // Take parsed string and map it to Pokemon Model class
+            Pokemon myPokemon = parsedString.ToObject<Pokemon>();
+
+            // Now we have an object that has all of the data of the pokemon we request
+            Debug.WriteLine(myPokemon.moves[0].move.name);
             return View();
         }
 
